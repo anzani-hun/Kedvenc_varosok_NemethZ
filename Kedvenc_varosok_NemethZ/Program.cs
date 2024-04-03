b2a91c433085271048fb29948dd3c396302ed5b6
anzani-hun <anzani@hun>
2024-04-03 17:38:52 +0200

    első mentés

--- Kedvenc_varosok_NemethZ/Program.cs
+++ Kedvenc_varosok_NemethZ/Program.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySqlConnector;   //ezt be kell tölteni
 
  class Program
  {
-        static void Main(string[] args)
-        {
+    static void Main(string[] args)
+    {
+        string connectionString;
+        MySqlConnection dbConnection;
+        MySqlCommand sqlCommand;
 
-        }
+        connectionString = "server=localhost;userid=root;password=;database=mo_telepulesek";
+        dbConnection = new MySqlConnection(connectionString);
+        dbConnection.Open();
+
+        sqlCommand = new MySqlCommand();
+        sqlCommand.Connection = dbConnection;
+        sqlCommand.CommandText = "DROP TABLE IF EXISTS kvarosok;";
+        sqlCommand.ExecuteNonQuery();
+
+        // SCALAR = egy visszatérési értéket várunk előző feladatban volt.
+        // EXECUTENONQUERY() = nem várom visszatérési értéket
+
+        // tábla létrehozása:
+
+
+        Console.ReadKey();
+    }
  }
